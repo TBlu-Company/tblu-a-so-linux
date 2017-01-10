0e0851f52285d351099f2f1f71a7100f418cba21
pauloferreira25 <paulo.ferreira25@gmail.com>
2017-01-10 18:32:25 -0200

    CPU

--- test/test-so.js
+++ test/test-so.js
@@ -1,11 +1,15 @@
 'use strict';
-
+const Datastore = require('nedb');
+const path = require('path');
+const dirname = path.dirname(__filename);
+const dBconfig = new Datastore(dirname + '/config.db');
+dBconfig.loadDatabase();
 const core = require('../index.js');
 
-describe('mOSCPU', function() {
-    it('get mOSCPU', function(done) {
+describe('mLinuxMemory', function() {
+    it('get mLinuxMemory', function(done) {
         let data = {};
-        data['moduleFunction'] = "mOSCPU";
+        data['moduleFunction'] = "mLinuxMemory";
         core.run(data).then(result => {
             console.log(result);
             done();
@@ -15,12 +19,12 @@ describe('mOSCPU', function() {
     });
 });
 
-describe('mLinuxMemory', function() {
-    it('get mLinuxMemory', function(done) {
+describe('mLinuxCPU', function() {
+    it('get mLinuxCPU', function(done) {
         let data = {};
-        data['moduleFunction'] = "mLinuxMemory";
-        core.run(data).then(result => {
-            console.log(result);
+        data['moduleFunction'] = "mLinuxCPU";
+        core.run(data, dBconfig).then(result => {
+            console.log(JSON.stringify(result));
             done();
         }).catch(error => {
             done(error);
